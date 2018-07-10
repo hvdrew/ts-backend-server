d1985066a67649e367f5a7fb387f5439ab8e4e90
Andrew <andrew.merison@gmail.com>
2018-07-09 22:40:48 -0700

    Added basic responses to test in production environment

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ let configurations: ConfigCollection = {
 // ================ //
 
 // Check if a configuration was set when the application started, if not then use development
-const chosenConfig = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV : 'development';
+const chosenConfig = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : 'development';
 
 // Use the config if it exists, or use the development
 const activeConfiguration = typeof(configurations[chosenConfig]) !== 'undefined' ? configurations[chosenConfig] : configurations.development;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-// import http = require('http');
+import http = require('http');
 import config = require('./config');
 
-console.log(config.default.PORT);
+const server = http.createServer((req, res) => {
+    res.end(`Hello! You visited the ${req.url} path of this api.`);
+});
+
+server.listen(config.default.PORT, (error) => {
+    if (error) {
+        return console.log('Something went wrong...', error);
+    }
+
+    console.log(`Server is listening on port ${config.default.PORT}`);
+});
