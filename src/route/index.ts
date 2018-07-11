e7d14234a5e63f9aa7af05db3c795e68ba8402a6
Andrew <andrew.merison@gmail.com>
2018-07-11 07:32:16 -0700

    Got routing working and rendered a template file successfully. Need to further expand the routing function.

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,15 +1,40 @@
-const placeholder = {};
+// import chooseClientRoute = require('./client/index');
+import renderHelper = require('../lib/render/index');
 
-// Not finished
 // Pass the trimmedPath into this function to filter traffic to it's designated handlers
 const chooseRoute = (path, data) => {
     if (path.indexOf('api') === 0) {
         // Pass data into API handler for further processing
+        let payload = {
+            'Requested Path': data.trimmedPath
+        };
+
+        let payloadString = JSON.stringify(payload);
+        let contentType = 'application/json';
+
+        let returnValue = {
+            'payload': payloadString,
+            'contentType': contentType
+        };
+        
+        return returnValue; 
     } else if (path.indexOf('admin') === 0) {
         // Pass data into Admin handler for further processing
     } else {
-        // Pass data into Client handler for further processing
+        // chooseClientRoute.default(path, data);
+        let payload = renderHelper.default('index', {
+            'path': path
+        });
+        let contentType = 'text/html';
+
+        let returnValue = {
+            'payload': payload,
+            'contentType': contentType
+        };
+
+        return returnValue;
+        // Render a template file for now
     }
 };
 
-export default placeholder;
+export default chooseRoute;
--- src/server.ts
+++ src/server.ts
@@ -9,36 +9,32 @@ const serverHandler = (req, res) => {
     let parsedUrl = url.parse(req.url, true);
     let _path = parsedUrl.pathname;
 
-    // Not used yet, but will be utilized for regulating paths and routing requests
     let trimmedPath = _path.replace(/^\/+|\/+$/g, '');
 
-    // TESTING PUG ENGINE, REMOVE ON PRODUCTION
-    // if (trimmedPath === 'test') {
-    //     console.log('test path triggered');
-    //     let file = path.join(__dirname, '../client/views/index.pug');
-    //     let renderedFile = pug.renderFile(file, {test: 'Hayo'});
-
-    //     res.setHeader('Content-Type', 'text/html');
-    //     res.writeHead(200);
-        
-    //     res.end(renderedFile);
-
-    //     return console.log('Rendered template for test path.');
-    // }
-    // END OF TESTS
-
-    let payload = {
-        'Requested Path': trimmedPath
+    let data = {
+        'trimmedPath': trimmedPath,
+        '_path': _path,
+        'parsedUrl': parsedUrl
     };
 
-    let payloadString = JSON.stringify(payload);
+    let headerData = router.default(data.trimmedPath, data);
 
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', headerData.contentType);
     res.writeHead(200);
+    res.end(headerData.payload);
 
-    res.end(payloadString);
+    // let payload = {
+    //     'Requested Path': trimmedPath
+    // };
 
-    console.log(payloadString);
+    // let payloadString = JSON.stringify(payload);
+
+    // res.setHeader('Content-Type', 'application/json');
+    // res.writeHead(200);
+
+    // res.end(payloadString);
+
+    // console.log(payloadString);
 };
 
 export default serverHandler;
