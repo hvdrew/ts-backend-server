7c5b17dc1af1fe2d1d448de814c5380f79dda583
Andrew <andrew.merison@gmail.com>
2018-07-11 05:35:31 -0700

    Minor changes to routing

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,3 +1,15 @@
 const placeholder = {};
 
+// Not finished
+// Pass the trimmedPath into this function to filter traffic to it's designated handlers
+const chooseRoute = (path, data) => {
+    if (path.indexOf('api') === 0) {
+        // Pass data into API handler for further processing
+    } else if (path.indexOf('admin') === 0) {
+        // Pass data into Admin handler for further processing
+    } else {
+        // Pass data into Client handler for further processing
+    }
+};
+
 export default placeholder;
