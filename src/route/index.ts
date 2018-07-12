bff3029972a61c51c90bb825b38fe355dac927b9
Andrew <andrew.merison@gmail.com>
2018-07-12 05:50:03 -0700

    Rebuilt the server to focus on serving up files as soon as possible. Adding a separate server instance later to handle API on a different port and subdomain.

--- src/route/index.ts
+++ src/route/index.ts
@@ -9,27 +9,39 @@ interface RequestData {
     fullPath: string;
     parsedUrl: url.UrlWithParsedQuery;
 }
+interface HeaderData {
+    payload: any;
+    contentType: string;
+};
 
 const baseViewPath = path.join(__dirname, '../../client/views/');
 
-const renderedViews = {
-    'index': pug.renderFile(baseViewPath + 'index.pug'),
-    '404': pug.renderFile(baseViewPath + '404.pug')
+// Let's make this into a key-object based map to allow for titles and other variables to be stored
+const viewMap = {
+    '/': 'index.pug',
+    '/about': 'about.pug',
+    '/contact': 'contact.pug'
 };
 
 const mainRouter = (data: RequestData) => {
-    switch ( data.fullPath ) {
-        case '/':
-            return {
-                'payload': renderedViews['index'],
-                'contentType': 'text/html'
-            }
-        default:
-            return {
-                'payload': renderedViews['404'],
-                'contentType': 'text/html'
-            }
-    }
+    
+    // Choose a view based on the requested path. If no view matches use the 404 handler
+    let chosenView = typeof(viewMap[data.fullPath]) == 'string' ? viewMap[data.fullPath] : '404.pug';
+
+    // Build out full path to view file
+    let viewPath = path.join(baseViewPath, chosenView);
+
+    // Render view with pug and prepare it as a payload
+    let payload = pug.renderFile(viewPath);
+
+    // Set up headerData
+    let headerData: HeaderData = {
+        'payload': payload,
+        'contentType': 'text/html'
+    };
+
+    return headerData;
+    
 };
 
 export default mainRouter;
